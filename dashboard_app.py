67b09271c1fa4b4ef61c57c3cc45ce0e0d4025e9
Pabloiaco <iacovone.pablo@gmail.com>
2025-05-28 10:44:16 -0300

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -22,4 +22,6 @@ def data():
     return jsonify(operations)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host="0.0.0.0", port=port) 
